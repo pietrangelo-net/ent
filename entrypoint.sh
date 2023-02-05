9fb78bae676a93472f0f418d9b7aa7ef88125612
pietrangelo <p.masala@entando.com>
2023-02-05 10:36:48 +0100

    chore: add other usefully outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Run ent commands
 $@
 
-# Retrieve the bundle name reading the entando.json file with jq
+# Get the bundle name from the entando.json file with jq
 bundle_name=$(jq -r '.name' entando.json)
 echo "bundle-name=$bundle_name" >> $GITHUB_OUTPUT
+
+# Get the bundle version from the entando.json file with jq
+bundle_version=$(jq -r '.version' entando.json)
+echo "bundle-version=$bundle_version" >> $GITHUB_OUTPUT
+
+# Get the list of microservices to build
+microservices_list=$(jq -r '.microservices[] | .name' entando.json | tr -d '\n')
+echo "microservices-list=$microservices_list" >> $GITHUB_OUTPUT
+
+#
